35d6c1f97e274a35c9b42927b9ff315b457cdb16
linmeng <1812323571@qq.com>
2019-01-15 13:55:54 +0800

    首次提交

---
+++ src/main/java/com/cropge/dao/ProductMapper.java
@@ -0,0 +1,17 @@
+package com.cropge.dao;
+
+import com.cropge.pojo.Product;
+
+public interface ProductMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Product record);
+
+    int insertSelective(Product record);
+
+    Product selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Product record);
+
+    int updateByPrimaryKey(Product record);
+}
